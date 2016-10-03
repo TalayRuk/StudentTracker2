987cd32e330690f73a51318f44a40291a57ef7ad
Jonathan Buchner <websites@jonathanbuchner.com>
2016-10-02 22:15:49 -0700

    Add starting files

---
+++ Tests/Course_Tests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Epicodus;
+
+namespace Tests
+{
+  public class Course_Tests : IDisposable
+  {
+    public Course_Tests()
+    {
+      string dataSource = "Data Source=(localdb)\\mssqllocaldb"; // Data Source identifies the server.
+      string databaseName = "epicodus_test"; // Initial Catalog is the database name
+      //Integrated Security sets the security of the database access to the Windows user that is currently logged in.
+      DBConfiguration.ConnectionString = ""+dataSource+";Initial Catalog="+databaseName+";Integrated Security=SSPI;";
+    }
+
+
+    public void Dispose()
+    {
+      // Item.DeleteAll();
+    }
+  }
+}

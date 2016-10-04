c59b77c2ce97b6023e30a2a5b75b82bafc035a15
Ian Wilcox and Russ Vetsper <ian+russ@>
2016-10-04 12:30:44 -0700

    create projects methods, all tests passing

--- Tests/Project_Tests.cs
+++ Tests/Project_Tests.cs
@@ -17,10 +17,51 @@ namespace Tests
       DBConfiguration.ConnectionString = ""+dataSource+";Initial Catalog="+databaseName+";Integrated Security=SSPI;";
     }
 
+    [Fact]
+    public void Test_CheckForEmptyDataBase()
+    {
+    int tableRows = Project.GetAll().Count;
+    Assert.Equal( 0, tableRows);
+    }
+
+    [Fact]
+    public void Test_checkGetNameFunction()
+    {
+      DateTime Date = new DateTime(2016,10,3);
+      Project newProject = new Project("ProjectName", Date);
+      Assert.Equal("ProjectName", newProject.GetName() );
+    }
+
+    [Fact]
+    public void Test_FindProject()
+    {
+      DateTime Date = new DateTime(2016,10,3);
+      Project newProject = new Project("ProjectName", Date);
+      newProject.Save();
+      Project foundProject = Project.Find( newProject.GetId() );
+      Assert.Equal(newProject, foundProject);
+    }
+    [Fact]
+    public void Test_DeleteOneProject()
+    {
+      DateTime Date = new DateTime(2016,10,3);
+      Project newProject = new Project("ProjectName", Date);
+      newProject.Save();
+
+      DateTime Date2 = new DateTime(2016,10,3);
+      Project newProject2 = new Project("ProjectName", Date2);
+      newProject2.Save();
+
+      newProject.Delete();
+
+      List<Project> allProjects = Project.GetAll();
+      List<Project> testProject = new List<Project> {newProject2};
+      Assert.Equal(testProject, allProjects);
+    }
 
     public void Dispose()
     {
-      // Item.DeleteAll();
+      Project.DeleteAll();
     }
   }
 }

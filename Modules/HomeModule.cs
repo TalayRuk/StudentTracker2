af6f3f52db1db1c061ce64a8d532f780e15f6fae
vp <notejde@gmail.com>
2016-10-05 14:36:14 -0700

    add update add homeModule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,6 +13,19 @@ namespace Epicodus
         Dictionary<string, object> model = ViewRoutes.IndexView();
         return View["index.cshtml", model];
       };
+      Get["/add"] = _ => {
+
+        string fname = Request.Form["fname"];
+        string fname = Request.Form["lname"];
+        string fname = Request.Form["email"];
+        string fname = Request.Form["picture"];
+        string fname = Request.Form["starDate"];
+        Student student = new Student (fname, lname, email, picture, startDate);
+        student.Save();
+
+        Dictionary<string, object> model = ViewRoutes.IndexView();
+        return View["index.cshtml", model];
+      }
 
     }
   }

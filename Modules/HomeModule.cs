7c928280be69fe54c9b830cc6604c8b8084c5ca5
iantwilcox91 <bunkadoo91@gmail.com>
2016-10-05 09:47:31 -0700

    Update HomeModule.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,24 +10,9 @@ namespace Epicodus
     {
 
       Get["/"] = _ => {
-
-
         Dictionary<string, object> model = ViewRoutes.IndexView();
         return View["index.cshtml", model];
       };
-
-
-        List<Student> studentList = Student.GetAll();
-        List<Course> courseList = Course.GetAll();
-        List<Project> projectList = Project.GetAll();
-        Dictionary<string, object> model = new Dictionary<string, object>{};
-        model.Add("studentList", studentList);
-        model.Add("courseList", courseList);
-        model.Add("projectList", projectList);
-
-        return View["index.cshtml", model];
-      };
- 
     }
   }
 }
